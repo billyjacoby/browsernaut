82d84912cd23708df0e2c3dc63035466c4f8ff5d
Billy Jacoby <billyjacoby@gmail.com>
2023-04-08 09:50:00 -0400

    add preferences window

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,8 +3,23 @@ use tauri_plugin_positioner::{Position, WindowExt};
 
 fn main() {
     let quit = CustomMenuItem::new("quit".to_string(), "Quit").accelerator("Cmd+Q");
-    let system_tray_menu = SystemTrayMenu::new().add_item(quit);
+    let preferences =
+        CustomMenuItem::new("preferences".to_string(), "Preferences").accelerator("Cmd+P");
+    let system_tray_menu = SystemTrayMenu::new().add_item(quit).add_item(preferences);
+
     tauri::Builder::default()
+        .setup(|app| {
+            // Build the preferences window here
+            tauri::WindowBuilder::new(
+                app,
+                "preferences_window",
+                tauri::WindowUrl::App("index.html".into()),
+            )
+            // Not visible by default
+            .visible(false)
+            .build()?;
+            Ok(())
+        })
         .plugin(tauri_plugin_positioner::init())
         .system_tray(SystemTray::new().with_menu(system_tray_menu))
         .on_system_tray_event(|app, event| {
@@ -15,12 +30,13 @@ fn main() {
                     size: _,
                     ..
                 } => {
-                    let window = app.get_window("main").unwrap();
+                    let window = app.get_window("menu_bar").unwrap();
                     // use TrayCenter as initial window position
                     let _ = window.move_window(Position::TrayCenter);
                     if window.is_visible().unwrap() {
                         window.hide().unwrap();
                     } else {
+                        // Need to hide the prefs window here
                         window.show().unwrap();
                         window.set_focus().unwrap();
                     }
@@ -28,6 +44,16 @@ fn main() {
                 SystemTrayEvent::MenuItemClick { id, .. } => match id.as_str() {
                     "quit" => {
                         std::process::exit(0);
+                    }
+                    "preferences" => {
+                        // Check if the menu window is open, if so we want to hide it
+                        let main_window = app.get_window("menu_bar").unwrap();
+                        if main_window.is_visible().unwrap() {
+                            main_window.hide().unwrap();
+                        }
+                        // Check if the preference window already exists, if so just focus on it
+                        let prefs_window = app.get_window("preferences_window").unwrap();
+                        prefs_window.show().unwrap();
                     }
                     _ => {}
                 },
--- src/App.tsx
+++ src/App.tsx
@@ -2,23 +2,33 @@ import React from 'react';
 import reactLogo from './assets/react.svg';
 import { Command } from '@tauri-apps/api/shell';
 import './App.css';
+import { getCurrent } from '@tauri-apps/api/window';
 
-import { getAllInstalledAppNames } from './main/utils/get-installed-app-names';
+// import { getAllInstalledAppNames } from './main/utils/get-installed-app-names';
+
+enum WindowLabelEnum {
+  MAIN = 'menu_bar',
+  PREFS = 'preferences_window',
+}
 
 function App() {
   const [appNames, setAppNames] = React.useState<string[]>([]);
+  const currentWindow: WindowLabelEnum = getCurrent().label as WindowLabelEnum;
+  console.log('currentWindow', currentWindow);
   const getAppNames = async () => {
     console.log('running chell command');
     const output = await new Command('findApps').execute();
     const newAppNames = output.stdout.trim().split('\n');
     console.log(newAppNames);
     setAppNames(newAppNames);
-    // getAllInstalledAppNames();
   };
 
   return (
     <div className="container">
       <h1>Welcome to Tauri!</h1>
+      <h3>
+        Currently in the <code>{currentWindow}</code> window
+      </h3>
 
       <div className="row">
         <a href="https://tauri.app" target="_blank">
