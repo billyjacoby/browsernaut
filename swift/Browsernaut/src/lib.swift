16860811ca178725322543f66ef490d2e044906b
Billy Jacoby <billyjacoby@gmail.com>
2023-05-03 23:18:25 -0400

    chore: fix versions

--- swift/Browsernaut/src/lib.swift
+++ swift/Browsernaut/src/lib.swift
@@ -8,6 +8,7 @@
 import SwiftRs
 import Foundation
 import AppKit
+import Cocoa
 
 @_cdecl("get_default_browser")
 public func getDefaultAppURL() -> SRString? {
@@ -27,13 +28,53 @@ public func setDefaultBrowser() -> Bool {
     
 }
 
-// TODO: implement function to get app icons for installed apps
-@_cdecl("get_file_thumbnail_base64")
-func getFileThumbnailBase64(path: SRString) -> SRString {
-    let path = path.toString();
+extension NSImage {
+    var base64String: String? {
+        guard let rep = NSBitmapImageRep(
+            bitmapDataPlanes: nil,
+            pixelsWide: Int(size.width),
+            pixelsHigh: Int(size.height),
+            bitsPerSample: 8,
+            samplesPerPixel: 4,
+            hasAlpha: true,
+            isPlanar: false,
+            colorSpaceName: .calibratedRGB,
+            bytesPerRow: 0,
+            bitsPerPixel: 0
+            ) else {
+                print("Couldn't create bitmap representation")
+                return nil
+        }
+
+        NSGraphicsContext.saveGraphicsState()
+        NSGraphicsContext.current = NSGraphicsContext(bitmapImageRep: rep)
+        draw(at: NSZeroPoint, from: NSZeroRect, operation: .sourceOver, fraction: 1.0)
+        NSGraphicsContext.restoreGraphicsState()
+
+        guard let data = rep.representation(using: NSBitmapImageRep.FileType.png, properties: [NSBitmapImageRep.PropertyKey.compressionFactor: 1.0]) else {
+            print("Couldn't create PNG")
+            return nil
+        }
+
+        // With prefix
+         return "data:image/png;base64,\(data.base64EncodedString(options: []))"
+    }
+}
+
+@_cdecl("get_app_icon")
+public func getAppIcon(appName: SRString, size: Int = 256) -> SRString? {
     
-    let image = NSWorkspace.shared.icon(forFile: path)
-    let bitmap = NSBitmapImageRep(data: image.tiffRepresentation!)!.representation(using: .png, properties: [:])!
+    if let path = NSWorkspace.shared.fullPath(forApplication: appName.toString()) {
+        if let rep = NSWorkspace.shared.icon(forFile: path)
+            .bestRepresentation(for: NSRect(x: 0, y: 0, width: size, height: size), context: nil, hints: nil) {
+            
+            let image = NSImage(size: rep.size)
+            image.addRepresentation(rep)
+            
+            return SRString(image.base64String ?? "")
+        }
+    }
     
-    return SRString(bitmap.base64EncodedString())
+    return SRString("")
+
 }
