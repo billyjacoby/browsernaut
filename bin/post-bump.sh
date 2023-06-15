cddd227dcc0e945e528d602628b55de9b998a456
Billy Jacoby <billyjacoby@gmail.com>
2023-06-14 21:34:40 -0400

    chore: update homebrew readme and script (#8)

--- bin/post-bump.sh
+++ bin/post-bump.sh
@@ -9,6 +9,8 @@ yarn build:release
 export UPDATE_SIG_FILE="./src-tauri/target/release/bundle/macos/Browsernaut.app.tar.gz.sig"
 export UPDATE_SIG=$(cat $UPDATE_SIG_FILE)
 
+export SHA_SUM=shasum -a 256 "./src-tauri/target/release/bundle/dmg/Browsernaut_$VERSION_x64.dmg"
+
 export PUBLISH_DATE=$(date -u -Iseconds | sed s/+00:00/Z/ | sed s/,/./)
 
 echo "Updating updates file"
@@ -23,6 +25,8 @@ sed -i "" "s/\"pub_date\":.*/\"pub_date\": \"$PUBLISH_DATE\",/" "./updates.json"
 ## HOMEBREW TAP UPDATES
 #? Update homebrew tap
 sed -i "" "s/$LATEST_VERSION/$VERSION/g" "./homebrew-browsernaut/Casks/browsernaut.rb"
+#? Update homebrew sha 
+sed -i "" "s/sha256:.*/\"sha256:\": \"$SHA_SUM\",/" "./homebrew-browsernaut/Casks/browsernaut.rb"
 #? Commit and push changes
 cd "./homebrew-browsernaut" && git add . && git commit -m "chore: update version to \"$LATEST_VERSION\""
 
