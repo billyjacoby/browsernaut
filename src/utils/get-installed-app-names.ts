c43991ecbd79c905340aea23643550f016b206c8
poegl.dev <poegl.dev@gmail.com>
2023-11-06 08:47:38 +0100

    refactor: Use Rust to get installed apps and consider also user apps

--- src/utils/get-installed-app-names.ts
+++ src/utils/get-installed-app-names.ts
@@ -1,12 +1,10 @@
-import { path } from '@tauri-apps/api';
-
 import { AppName, apps } from '@config/apps';
-import { Command } from '@tauri-apps/api/shell';
+import { path } from '@tauri-apps/api';
+import { invoke } from '@tauri-apps/api/tauri'
 
 async function getAllInstalledAppNames(): Promise<string[]> {
-  const output = await new Command('findApps').execute();
-  const appNamesRaw = output.stdout.trim().split('\n');
-  const appNamePromises = appNamesRaw.map(async (appPath) => {
+  const apps = await invoke('get_apps') as Array<string>
+  const appNamePromises = apps.map(async (appPath) => {
     const baseName = await path.basename(appPath);
     const appName = baseName.substring(0, baseName.length - 4);
     return appName;
